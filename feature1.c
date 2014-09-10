0793ab38a18cc333f5ebd2b42479f9c0993a68a4
Jonathon Reinhart <Jonathon.Reinhart@gmail.com>
2014-09-10 02:43:18 -0400

    Add feature1 code

---
+++ feature1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "feature1.h"
+
+int feature1(void)
+{
+    printf("This is feature1\n");
+    return 0;
+}
---
+++ feature1.h
@@ -0,0 +1,6 @@
+#ifndef __FEATURE1_H
+#define __FEATURE1_H
+
+int feature1(void);
+
+#endif /* __FEATURE1_H */
