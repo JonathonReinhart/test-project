500ea11c229a001709d7f1bd8adf72ebf62fbf50
Jonathon Reinhart <Jonathon.Reinhart@gmail.com>
2014-09-10 02:47:35 -0400

    Add feature2 code

---
+++ feature2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "feature2.h"
+
+int feature2(void)
+{
+    printf("Here's feature2, which is also great.\n");
+    return 0;
+}
---
+++ feature2.h
@@ -0,0 +1,6 @@
+#ifndef __FEATURE2_H
+#define __FEATURE2_H
+
+int feature2(void);
+
+#endif /* __FEATURE2_H */
