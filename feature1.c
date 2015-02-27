cbc4921afe2028266a63ec30cdea8ea505060b18
Jonathon Reinhart <jonathon.reinhart@gmail.com>
2015-02-27 04:49:41 -0500

    added comment

--- feature1.c
+++ feature1.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include "feature1.h"
 
+/**
+ * I'm adding a comment to see if syntax highlighting still
+ * sucks balls, or if GitLab 7.8.1 fixes this.
+ * One more line.
+ */
 int feature1(void)
 {
     printf("This is feature1 -- Shiny!\n");
