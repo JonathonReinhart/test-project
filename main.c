3704a5525f244c001c877e00b1966a0bdb5063b9
Jonathon Reinhart <Jonathon.Reinhart@gmail.com>
2016-04-10 17:41:38 -0400

    Call feature3 from main

--- main.c
+++ main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include "feature1.h"
 #include "feature2.h"
+#include "feature3.h"
 
 int main(void)
 {
     printf("What's a project without 'int main'?\n");
     feature1();
     feature2();
+    feature3();
 
     return 0;
 }
